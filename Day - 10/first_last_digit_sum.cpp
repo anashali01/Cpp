866ac952e41f4956d5e303444389b7436d9535b3
anashali01 <saiyedanashali@gmail.com>
2025-04-16 10:31:20 +0530

    crud complete

--- Day - 10/first_last_digit_sum.cpp
+++ Day - 10/first_last_digit_sum.cpp
@@ -1,19 +1,29 @@
+// Program to find sum of first and last digits of a number
+
 #include <iostream>
 using namespace std;
 int main()
 {
-    int num , fd , ld ;
+    // Variables to store the number and its digits
+    int num,    // Input number from user
+        fd,     // First digit
+        ld;     // Last digit
 
+    // Get the number from user
     cout << "Enter Number :";
     cin >> num;
 
+    // Extract last digit using modulo operator (remainder when divided by 10)
     ld = num % 10;
+
+    // Loop to find first digit by continuously dividing by 10
     while (num != 0)
     {
-        fd = num % 10;
-        num /= 10;
-
+        fd = num % 10;    // Get current last digit
+        num /= 10;        // Remove last digit
     }
+
+    // Display sum of first and last digits
     cout << "The sum of first and last digit :" << ld + fd;
     return 0;
 }
---
+++ Day - 12/crud.cpp
@@ -0,0 +1,56 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main()
+{
+    int size, choice, index, element;
+    cout << "Enter size of array :";
+    cin >> size;
+    int arr[size];
+    do
+    {
+        cout << "-- Enter 0 for EXIT --" << endl;
+        cout << "-- Enter 1 for INSERT --" << endl;
+        cout << "-- Enter 2 for UPDATE --" << endl;
+        cout << "-- Enter 3 for DELETE --" << endl;
+        cout << "-- Enter 4 for VIEW --" << endl;
+
+        cout << "Enter Your Choice :";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            for (int i = 0; i < size; i++)
+            {
+                cout << "Enter The Element arr[" << i << "]" << ": ";
+                cin >> arr[i];
+            }
+            break;
+        case 2:
+            cout << "Enter Your index :";
+            cin >> index;
+            cout << "Enter Your element :";
+            cin >> element;
+            arr[index] = element;
+            break;
+        case 3:
+            cout << "Enter index for delete :";
+            cin >> index;
+            for (int i = index; i < size; i++)
+            {
+                arr[i] = arr[i + 1];
+            }
+            size--;
+            break;
+        case 4:
+            for (int i = 0; i < size; i++)
+            {
+                cout << "element of a[" << i << "]: " << arr[i] << endl;
+            }
+            break;
+        }
+    } while (choice != 0);
+
+    return 0;
+}
